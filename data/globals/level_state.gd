82f5ed155f38a55a5ed2db1d37180c4507e30462
Jason <j989li@uwaterloo.ca>
2024-12-19 11:24:26 -0500

    added game timer

--- data/globals/level_state.gd
+++ data/globals/level_state.gd
@@ -1,8 +1,11 @@
 extends Node
 
+var game_time: float = 0
+
 var player_gold = 50
 var player_gold_gen = 5
 
 func reset_level_state() -> void:
+	game_time = 0
 	player_gold = 50
 	player_gold_gen = 5
--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -1,7 +1,16 @@
-extends Node
+class_name Level1 extends Node
+
+@onready var game_time = $UI/GameTime
 
 func _ready() -> void:
 	_init_level()
 
+func _process(delta: float) -> void:
+	_update_game_time(delta)
+
 func _init_level() -> void:
-	pass
+	game_time.text = Utils.format_time(0)
+
+func _update_game_time(delta) -> void:
+	LevelState.game_time += delta
+	game_time.text = Utils.format_time(LevelState.game_time)
