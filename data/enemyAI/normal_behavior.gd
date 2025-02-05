e1d0a59979a74d1f6e2ef8573c5719fdd389c48a
Jason <j989li@uwaterloo.ca>
2025-02-05 06:38:29 -0500

    fixed ai behavior

--- data/enemyAI/normal_behavior.gd
+++ data/enemyAI/normal_behavior.gd
@@ -8,7 +8,7 @@ func init(enemy_ai: EnemyAI) -> void:
 	target_gold_gen_time_step = 25
 	
 	ai.enemy_gold *= LevelState.level_number
-	ai.enemy_gold_gen = max(4 + _current_level, floor(GameState.total_game_time / (_enemy_gold_gen_step + 10 * _current_level)))
+	ai.enemy_gold_gen = max(4, floor(GameState.total_game_time / (_enemy_gold_gen_step * _current_level)))
 
 func generate_decision_wait_time() -> float:
 	return randf_range(0.2, 0.75)
